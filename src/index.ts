ee2cbf8efa908b2fdfb77780c4f2193a6224d263
Ivan Hell <ivan.hell@innovaalp.com>
2017-10-30 18:23:57 +0100

    feat: add method for simply upgrading a collection (by name)
    BREAKING CHANGE: migrateDb was renamed to migrateModel
    
    * writeVersion/readVersion now only accept collection names
    * interface Migration was renamed to ModelMigrationHandler
    * signature for up/down function of ModelMigrationHandler has changed (last two parameters are now fromVersion, toVersion)

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,133 @@
 import {connection, Model} from 'mongoose';
 
+export interface CollectionVersion {
+    _id: string;
+    updated: Date;
+    current: number;
+    last: number;
+}
 
-function getVersionCollection(model: Model<any>) {
+function getVersionCollection(collectionName: string) {
     const db = connection.db;
-    const collectionName = model.collection.name;
     return db.collection(`${collectionName}.version`);
 }
 
-export function writeVersion(model: Model<any>, version: number): Promise<any> {
-    let update = {
-        current: version,
-        updated: new Date()
+export function writeVersion(collectionName: string, version: number): Promise<CollectionVersion> {
+    let update: any = {
+	current: version,
+	updated: new Date()
     };
 
-    return readVersion(model)
-        .then(currentVersion => {
-            if(!currentVersion) return getVersionCollection(model).insertOne(update);
+    return readVersion(collectionName)
+	.then(currentVersion => {
+	    if(!currentVersion) return getVersionCollection(collectionName).insertOne(update);
 
-            update['last'] = currentVersion.current;
+	    update['last'] = currentVersion.current;
 
-            getVersionCollection(model).findOneAndUpdate({_id: currentVersion._id}, update);
-        });
+	    return getVersionCollection(collectionName)
+		.findOneAndUpdate({_id: currentVersion._id}, update);
+	});
 }
 
-export function readVersion(model: Model<any>): Promise<any> {
-    return getVersionCollection(model)
-        .findOne({});
+export function readVersion(collectionName: string): Promise<CollectionVersion> {
+    return getVersionCollection(collectionName)
+	.findOne({});
 }
 
-export interface Migration {
-    up(db: any, model: Model<any>, version: number): Promise<any>;
-    down(db: any, model: Model<any>, version: number): Promise<any>;
+
+abstract class AbstractMigrator {
+
+    constructor(
+	protected collectionName: string
+    ) {}
+
+    public migrate(version: number): Promise<any> {
+	return readVersion(this.collectionName)
+	    .then(currentVersion => {
+		if(currentVersion && currentVersion.current === version) return;
+
+		// no version available
+		const current = currentVersion && currentVersion.current;
+
+		if(!current || current < version) {
+		    return this.upgrade(current, version);
+		} else {
+		    return this.downgrade(current, version);
+		}
+	    })
+	    .then(() => writeVersion(this.collectionName, version));
+    }
+
+    protected getCollection() {
+	return connection.db.collection(this.collectionName);
+    }
+
+    protected abstract upgrade(fromVersion: number, toVersion: number);
+
+    protected abstract downgrade(fromVersion: number, toVersion: number);
 }
 
-export function migrateDb(model: Model<any>, version: number, migration: Migration): Promise<any> {
-    return readVersion(model)
-        .then(currentVersion => {
-            if(currentVersion && currentVersion.current === version) return;
 
-            // no version available
-            const current = currentVersion && currentVersion.current;
 
-            if(!current || current < version) {
-                return migration.up(connection.db, model, version);
-            } else {
-                throw new Error(`Downgrading model version from ${currentVersion.current} to ${version} not supported yet!`);
-            }
-        })
-        .then(() => writeVersion(model, version));
+export interface CollectionMigrationHandler {
+    up(db: any, collection: any, fromVersion: number, toVersion: number): Promise<any>;
+    down(db: any, collection: any, fromVersion: number, toVersion: number): Promise<any>;
+}
+
+
+class CollectionMigrator extends AbstractMigrator {
+
+    constructor(
+	collectionName: string,
+	private migrationHandler: CollectionMigrationHandler
+    ) {
+	super(collectionName);
+    }
+
+    protected upgrade(fromVersion: number, toVersion: number) {
+	return this.migrationHandler.up(connection.db, this.getCollection(), fromVersion, toVersion);
+    }
+
+    protected downgrade(fromVersion: number, toVersion: number) {
+	return Promise.reject(new Error(`Downgrading a collection from version ${fromVersion} to ${toVersion} not supported yet!`));
+    }
+
+}
+
+
+export interface ModelMigrationHandler {
+    up(db: any, model: Model<any>, fromVersion: number, toVersion: number): Promise<any>;
+    down(db: any, model: Model<any>, fromVersion: number, toVersion: number): Promise<any>;
+}
+
+
+class ModelMigrator extends AbstractMigrator {
+
+    constructor(
+	private model: Model<any>,
+	private migrationHandler: ModelMigrationHandler
+    ) {
+	super(model.collection.name);
+    }
+
+    protected upgrade(fromVersion: number, toVersion: number) {
+	return this.migrationHandler.up(connection.db, this.model, fromVersion, toVersion);
+    }
+
+    protected downgrade(fromVersion: number, toVersion: number) {
+	return Promise.reject(new Error(`Downgrading a model version from ${fromVersion} to ${toVersion} not supported yet!`));
+    }
+
+}
+
+
+export function migrateModel(model: Model<any>, version: number, migrationHandler: ModelMigrationHandler): Promise<any> {
+    const migrator = new ModelMigrator(model, migrationHandler);
+    return migrator.migrate(version);
+}
+
+
+export function migrateCollection(collectionName: string, version: number, migrationHandler: CollectionMigrationHandler): Promise<any> {
+    const migrator = new CollectionMigrator(collectionName, migrationHandler);
+    return migrator.migrate(version);
 }
